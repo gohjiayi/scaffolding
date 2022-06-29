0432910ab1bcde4ec7d69df8d170989b41c294f8
Goh Jia Yi <gooohjiayi@gmail.com>
2022-06-29 08:16:07 +0000

    add setup files

---
+++ hello.py
@@ -0,0 +1,14 @@
+def add(x, y):
+    return x + y
+
+
+print(f"This is the sum: 1, 2, {add(1,2)}")
+
+# def toyou(x):
+#     return f"hi {x}"
+
+# def add(x):
+#     return x + 1
+
+# def subtract(x):
+#     return x - 1
